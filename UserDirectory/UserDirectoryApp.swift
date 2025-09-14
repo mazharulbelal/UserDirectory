30e3c1e8ff3d8b9e078ad7ac57bcda5e500c6a61
Mazharul <mazharul.belal@gmail.com>
2025-09-14 07:19:59 +0600

    Initial Commit

---
+++ UserDirectory/UserDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDirectoryApp.swift
+//  UserDirectory
+//
+//  Created by Md Mazharul Islam on 14/9/25.
+//
+
+import SwiftUI
+
+@main
+struct UserDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
